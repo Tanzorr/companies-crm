60b231efe09989e422ad7b5022d3a2ded25bc60e
olexandr <alexx1984@ukr.net>
2024-02-19 10:16:01 +0200

    add table style

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -7,39 +7,39 @@
                 <h1 class="text-xl mb-4">Company List</h1>
                 <a href="{{ route('companies.create') }}" class="btn btn-primary">Create Company</a>
             </div>
-            <table class="mt-5">
+            <table class="min-w-full mt-5 bg-white border border-gray-300">
                 <thead>
                 <tr>
-                    <th>Logo</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Website</th>
-                    <th>Action</th>
+                    <th class="py-2 px-4 border-b">Logo</th>
+                    <th class="py-2 px-4 border-b">Name</th>
+                    <th class="py-2 px-4 border-b">Email</th>
+                    <th class="py-2 px-4 border-b">Website</th>
+                    <th class="py-2 px-4 border-b">Action</th>
                 </tr>
                 </thead>
                 <tbody>
                 @forelse($companies as $company)
                     <tr>
-                        <td><img src="{{ asset('/storage/logos/'.$company->logo) }}" alt="" width=100 height=100></td>
-                        <td>{{ $company->name }}</td>
-                        <td>{{ $company->email }}</td>
-                        <td>
-                            <x-link-button href="{{ $company->website }}">Visit</x-link-button>
+                        <td class="py-2 px-4 border-b"><img src="{{ asset('/storage/logos/'.$company->logo) }}" alt="" width="100" height="100"></td>
+                        <td class="py-2 px-4 border-b">{{ $company->name }}</td>
+                        <td class="py-2 px-4 border-b">{{ $company->email }}</td>
+                        <td class="py-2 px-4 border-b">
+                            <x-link-button href="{{ $company->website }}" class="text-blue-500">Visit</x-link-button>
                         </td>
-                        <td>
+                        <td class="py-2 px-4 border-b">
                             <div class="flex gap-2">
-                                <x-link-button href="{{route('companies.edit', $company)}}">Edit</x-link-button>
+                                <x-link-button href="{{ route('companies.edit', $company) }}" class="text-blue-500">Edit</x-link-button>
                                 <form action="{{ route('companies.destroy', $company) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
-                                    <x-button >Delete</x-button>
+                                    <x-button class="text-red-500">Delete</x-button>
                                 </form>
                             </div>
                         </td>
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="5">No Company Found</td>
+                        <td colspan="5" class="py-2 px-4 border-b">No Company Found</td>
                     </tr>
                 @endforelse
                 </tbody>
--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -8,42 +8,43 @@
                 <a href="{{ route('employees.create') }}" class="btn btn-primary">Add Employee</a>
             </div>
         </div>
-        <table class="mt-5">
+        <table class="min-w-full mt-5 bg-white border border-gray-300">
             <thead>
             <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Company</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Action</th>
+                <th class="py-2 px-4 border-b">First Name</th>
+                <th class="py-2 px-4 border-b">Last Name</th>
+                <th class="py-2 px-4 border-b">Company</th>
+                <th class="py-2 px-4 border-b">Email</th>
+                <th class="py-2 px-4 border-b">Phone</th>
+                <th class="py-2 px-4 border-b">Action</th>
             </tr>
             </thead>
             <tbody>
-                @forelse($employees as $employee)
-                    <tr>
-                        <td>{{ $employee->first_name }}</td>
-                        <td> {{ $employee->last_name }}</td>
-                        <td> {{ $employee->company->name }}</td>
-                        <td> {{ $employee->email }}</td>
-                        <td> {{ $employee->phone }}</td>
-                        <td>
-                            <div class="flex gap-2">
-                                <x-link-button href="{{route('employees.edit', $employee)}}">Edit</x-link-button>
-                                <form action="{{ route('employees.destroy', $employee) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <x-button >Delete</x-button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                @empty
+            @forelse($employees as $employee)
                 <tr>
-                    <td colspan="6">No Employee Found</td>
+                    <td class="py-2 px-4 border-b">{{ $employee->first_name }}</td>
+                    <td class="py-2 px-4 border-b">{{ $employee->last_name }}</td>
+                    <td class="py-2 px-4 border-b">{{ $employee->company->name }}</td>
+                    <td class="py-2 px-4 border-b">{{ $employee->email }}</td>
+                    <td class="py-2 px-4 border-b">{{ $employee->phone }}</td>
+                    <td class="py-2 px-4 border-b">
+                        <div class="flex gap-2">
+                            <x-link-button href="{{ route('employees.edit', $employee) }}" class="text-blue-500">Edit</x-link-button>
+                            <form action="{{ route('employees.destroy', $employee) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <x-button class="text-red-500">Delete</x-button>
+                            </form>
+                        </div>
+                    </td>
                 </tr>
-                @endforelse
+            @empty
+                <tr>
+                    <td colspan="6" class="py-2 px-4 border-b">No Employee Found</td>
+                </tr>
+            @endforelse
             </tbody>
         </table>
+
     </div>
 @endsection
